e907a2bd2ba8135f03ad889251c4349a2b281c69
haitran0366 <haitran_usa@yahoo.com>
2021-09-26 18:11:41 -0700

    Add project files.

---
+++ DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFPart11
+{
+    internal class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+        public DelegateCommand(Action action, Func<bool> canExecute)
+        {
+            _execute = action;
+            _canExecute = canExecute;
+        }
+        public DelegateCommand(Action action):this(action,() => true)
+        {
+            _execute = action;
+        }
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ HelloViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFPart11
+{
+    public class HelloViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string name ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string name;
+        public string Name 
+        {
+            get{ return name; }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        private string greeting;
+        public string Greeting
+        {
+            get { return greeting; }
+            set
+            {
+                greeting = value;
+                NotifyPropertyChanged("Greeting");
+            }
+        }
+
+        public ICommand cmdSubmitName { get; set; }
+        public bool CanExecuteSubmit 
+        {
+            get { return !string.IsNullOrEmpty(Name); }
+
+        }
+
+        public HelloViewModel()
+        {
+            cmdSubmitName = new DelegateCommand(ProcessSubmit, () => CanExecuteSubmit);
+        }
+
+        private void ProcessSubmit()
+        {
+            Greeting = $"Hello {Name}";
+        }
+    }
+}
